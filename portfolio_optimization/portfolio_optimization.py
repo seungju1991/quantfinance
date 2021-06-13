109328c9d8ab6e9ec2c80c3f4fa6c3998cf42479
Seungju Seo <seungjuseo@Seungjus-Mac-mini.local>
2021-06-13 20:16:58 +0900

    revised python file

--- portfolio_optimization/portfolio_optimization.py
+++ portfolio_optimization/portfolio_optimization.py
@@ -4,5 +4,14 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import time
 import seaborn as sns
+sns.set()
+plt.style.use('ggplot')
+plt.rcParams['axes.prop_cycle'] 
+font = {'family' : 'sans-serif'}
+matplotlib.rc('font', **font)
+import matplotlib.dates as mdates
+import scipy.optimize as sco
+import scipy.special as scs
+pd.set_option("display.max_columns", 20)
 
 print("hello")
