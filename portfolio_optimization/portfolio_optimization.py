9382f809a5dafbc05a5363978660a66057e95859
Seungju Seo <seungjuseo@Seungjus-Mac-mini.local>
2021-06-13 20:36:38 +0900

    update

--- portfolio_optimization/portfolio_optimization.py
+++ portfolio_optimization/portfolio_optimization.py
@@ -8,10 +8,19 @@ sns.set()
 plt.style.use('ggplot')
 plt.rcParams['axes.prop_cycle'] 
 font = {'family' : 'sans-serif'}
-matplotlib.rc('font', **font)
 import matplotlib.dates as mdates
 import scipy.optimize as sco
 import scipy.special as scs
 pd.set_option("display.max_columns", 20)
 
 print("hello")
+
+
+def main():
+    print("hi")
+
+
+if __name__ == "__main__":
+    main()
+
+
